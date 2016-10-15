12f2d9fe02baefa562b3ca193ea6b1fb2b790067
Vladimir Stavitskiy <stavytskyivolodymyr@gmail.com>
2016-10-15 10:30:17 +0300

    battleship game

---
+++ battleship/battleship.js
@@ -0,0 +1,175 @@
+//MVC, Model/View/Controller
+//Объект Представление
+var view = {
+	displayMessage: function(msg) { 
+		var messageArea = document.getElementById("messageArea");
+		messageArea.innerHTML = msg;
+	},
+	displayHit: function(location) {
+		var cell = document.getElementById(location);
+		//Идентификатор, созданный по введенным пользователем координатам,
+		//используется для получения обновляемого элемента.
+		cell.setAttribute('class', 'hit');
+		//этому элементу назначается класс 'hit'
+	},
+	displayMiss: function(location) {
+		var cell = document.getElementById(location);
+		cell.setAttribute('class', 'miss');
+	}
+};
+
+
+
+<!-- / ========= Объект Модели ========= \ -->
+var model = {
+   boardSize: 7,
+   numShips: 3,
+   shipsSunk: 0,
+   shipLength: 3,   
+   ships: [{ locations: [0, 0, 0], hits: ["", "", ""] },
+           { locations: [0, 0, 0], hits: ["", "", ""] },
+           { locations: [0, 0, 0], hits: ["", "", ""] }],
+  	
+   fire: function(guess) {
+   		for (var i = 0; i < this.numShips; i++) {
+   			var ship = this.ships[i];
+   			var index = ship.locations.indexOf(guess);
+
+   			if (index >= 0) {
+   				ship.hits[index] = "hit";
+   				view.displayHit(guess);
+   				view.displayMessage("HIT!!!");
+   				if (this.isSunk(ship)) {
+   					view.displayMessage("You sunk my battleship!");
+   					this.shipsSunk++;
+   				}
+   				return true;
+   			}
+   		}
+   		view.displayMiss(guess);
+   		view.displayMessage("You missed!");
+   		return false;
+
+   },
+   isSunk: function(ship) {
+   	for (var i = 0; i<this.shipLength; i++) {
+   		if (ship.hits[i] !== "hit") {
+   			return false;
+   		}
+   	}
+   	return true;
+   },
+
+   generateShipLocations: function() {
+	    var locations;
+	    for (var i = 0; i < this.numShips; i++) {
+	        do {
+	            locations = this.generateShip();
+	        } while (this.collision(locations));
+	        this.ships[i].locations = locations;
+	    }
+	},
+
+	generateShip: function() {
+    var direction = Math.floor(Math.random() * 2);
+    var row, col;
+    //если направление горизонтальное, то:
+    if (direction === 1) { 
+        row = Math.floor(Math.random() * this.boardSize);
+        col = Math.floor(Math.random() * (this.boardSize - this.shipLength+1));
+    } else { 
+        row = Math.floor(Math.random() * (this.boardSize - this.shipLength+1));
+        col = Math.floor(Math.random() * this.boardSize);
+    }
+    //Набор позиций нового корабля
+    var newShipLocations = [];
+    for (var i = 0; i < this.shipLength; i++) {
+        if (direction === 1) {
+            newShipLocations.push(row + "" + (col + i));
+		} else {
+        newShipLocations.push((row + i) + "" + col);
+    	}
+    }
+	    return newShipLocations;
+	},
+
+	//Проверка на столкновения
+	collision: function(locations) {
+		for (var i = 0; i < this.numShips; i++) {
+			var ship = model.ships[i];
+			for (j = 0; j < locations.length; j++) {
+				if (ship.locations.indexOf(locations[j]) >= 0) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+};
+
+
+
+<!-- / ========= Объект Контроллера ========= \ -->
+var controller = {
+	guesses: 0,
+
+	processGuess: function(guess) {
+		var location = parseGuess(guess);
+		if (location) {
+			this.guesses++;
+			var hit = model.fire(location);
+			if (hit && model.shipsSunk === model.numShips) {
+				view.displayMessage("You sunk all my batleships, in " + this.guesses + " guesses!");
+			}
+		}
+
+		function parseGuess(guess) {
+		var alphabet = ["A", "B", "C", "D", "E", "F", "G"];
+		if (guess === null || guess.length !== 2) {
+			alert("Oops, please ener a letter and a number on the board!");
+		}	else {
+			firtChar = guess.charAt(0);
+			var row = alphabet.indexOf(firtChar);
+			var column = guess.charAt(1);
+
+			if (isNaN(row) || isNaN(column)) {
+				alert("Oops it isn't on the board!");
+			} else if (row < 0 || row >= model.boardSize ||
+					column <0 || column >= model.boardSize) {
+					alert("Oop it's off the board!");
+				} else {
+					return row+column;
+				}
+		}
+		return null;
+	}
+
+	},	
+};
+
+
+function init() {
+	var fireButton = document.getElementById("fireButton");
+	fireButton.onclick = handleFireButton;
+	var guessInput = document.getElementById("guessInput");
+   	guessInput.onkeypress = handleKeyPress;
+   	//генерировать корабли
+   	model.generateShipLocations();
+}
+
+function handleFireButton() {
+	var guessInput = document.getElementById("guessInput");
+    var guess = guessInput.value;
+    controller.processGuess(guess);
+    guessInput.value = "";
+}
+window.onload = init;
+
+//при нажатии на клавишу Enter срабатывает кнопка "Fire"
+function handleKeyPress(e) {
+   var fireButton = document.getElementById("fireButton");
+   if (e.keyCode === 13) {
+      fireButton.click();
+      return false;
+   }
+}
